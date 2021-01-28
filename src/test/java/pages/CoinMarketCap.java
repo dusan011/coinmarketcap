4cc130b13e4e416e87908c78ad41143033ee125b
iamdusan <dusnen@gmail.com>
2021-01-29 00:05:20 +0100

    implemented a method for closing popups

--- src/test/java/pages/CoinMarketCap.java
+++ src/test/java/pages/CoinMarketCap.java
@@ -1,8 +1,11 @@
 package pages;
 
 import helpers.BaseHelper;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class CoinMarketCap extends BaseHelper
 {
@@ -18,8 +21,18 @@ public class CoinMarketCap extends BaseHelper
         driver.get(url);
     }
 
+    private void closePopUps()
+    {
+        wdWait.until(ExpectedConditions.presenceOfElementLocated(By.className("cmc-button-plain")));
+        WebElement questionPopUp = driver.findElement(By.className("cmc-button-plain"));
+        questionPopUp.click();
+        WebElement cookiePopUp = driver.findElement(By.className("cmc-cookie-policy-banner__close"));
+        cookiePopUp.click();
+    }
+
     public void coinMarketCap()
     {
         navigateToCoinMarketCap("https://coinmarketcap.com/");
+        closePopUps();
     }
 }
